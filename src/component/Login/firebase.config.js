154b30379c657284dea96dddda115e09cea97a1b
mdrefathossen <refathossen2001@gmail.com>
2021-07-05 16:03:41 +0600

    some change

--- src/component/Login/firebase.config.js
+++ src/component/Login/firebase.config.js
@@ -1,1 +1,12 @@
-ignore this file
+import React from 'react';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBooVOe__TyOMLZt6Qu-3FUvCUTCjC5E04",
+    authDomain: "firsttest-b640e.firebaseapp.com",
+    projectId: "firsttest-b640e",
+    storageBucket: "firsttest-b640e.appspot.com",
+    messagingSenderId: "133361803999",
+    appId: "1:133361803999:web:7e18083cfd28ed7710ff6a"
+  };
+
+export default firebaseConfig;
